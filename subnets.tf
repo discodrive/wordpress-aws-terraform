b0b066891be4a5152f053827a92b06842d56d700
Lee Aplin <lee@substrakt.com>
2023-04-17 14:25:17 +0100

    UPDATE: Terraform fmt

--- subnets.tf
+++ subnets.tf
@@ -1,34 +1,34 @@
 # Public Subnets
 resource "aws_subnet" "public_primary" {
-    vpc_id = aws_vpc.main.id
-    cidr_block = "10.0.0.0/24"
-    availability_zone = data.aws_availability_zones.available.names[0]
-    map_public_ip_on_launch = true
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.0.0/24"
+  availability_zone       = data.aws_availability_zones.available.names[0]
+  map_public_ip_on_launch = true
 
-    tags = {
-        Name = "Public Subnet 1"
-    }
+  tags = {
+    Name = "Public Subnet 1"
+  }
 }
 
 resource "aws_subnet" "public_secondary" {
-    vpc_id = aws_vpc.main.id
-    cidr_block = "10.0.1.0/24"
-    availability_zone = data.aws_availability_zones.available.names[1]
-    map_public_ip_on_launch = true
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = data.aws_availability_zones.available.names[1]
+  map_public_ip_on_launch = true
 
-    tags = {
-        Name = "Public Subnet 2"
-    }
+  tags = {
+    Name = "Public Subnet 2"
+  }
 }
 
 # Private Subnets
 resource "aws_subnet" "private_subnets" {
-    count = length(var.private_subnets)
-    vpc_id = aws_vpc.main.id
-    cidr_block = var.private_subnets[count.index].cidr_block
-    availability_zone = data.aws_availability_zones.available.names[var.private_subnets[count.index].availability_zone]
+  count             = length(var.private_subnets)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnets[count.index].cidr_block
+  availability_zone = data.aws_availability_zones.available.names[var.private_subnets[count.index].availability_zone]
 
-    tags = {
-        Name = var.private_subnets[count.index].name_tag
-    }
+  tags = {
+    Name = var.private_subnets[count.index].name_tag
+  }
 }
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,23 +1,22 @@
 private_subnets = [
-    {
-        cidr_block = "10.0.2.0/24"
-        availability_zone = "0"
-        name_tag = "Private Subnet 1 - App Tier"
-    },
-    {
-        cidr_block = "10.0.3.0/24"
-        availability_zone = "0"
-        name_tag = "Private Subnet 2 - App Tier"
-    },
-    {
-        cidr_block = "10.0.4.0/24"
-        availability_zone = "1"
-        name_tag = "Private Subnet 3 - DB Tier"
-    },
-    {
-        cidr_block = "10.0.5.0/24"
-        availability_zone = "1"
-        name_tag = "Private Subnet 4 - DB Tier"
-    }
-    
+  {
+    cidr_block        = "10.0.2.0/24"
+    availability_zone = "0"
+    name_tag          = "Private Subnet 1 - App Tier"
+  },
+  {
+    cidr_block        = "10.0.3.0/24"
+    availability_zone = "0"
+    name_tag          = "Private Subnet 2 - App Tier"
+  },
+  {
+    cidr_block        = "10.0.4.0/24"
+    availability_zone = "1"
+    name_tag          = "Private Subnet 3 - DB Tier"
+  },
+  {
+    cidr_block        = "10.0.5.0/24"
+    availability_zone = "1"
+    name_tag          = "Private Subnet 4 - DB Tier"
+  }
 ]
