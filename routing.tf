1d700676cfc8fe5a9e68aad0e875934e7f3591b7
Lee Aplin <lee@substrakt.com>
2023-06-06 18:40:39 +0100

    ADD: Security groups and ingress rules for EFS, Database, Webserver and ALB

--- routing.tf
+++ routing.tf
@@ -12,7 +12,7 @@ resource "aws_default_route_table" "public_route_table" {
 resource "aws_route_table" "private_route_table_1" {
   vpc_id = aws_vpc.main.id
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.nat_gateway_1.id
   }
 }
@@ -20,7 +20,7 @@ resource "aws_route_table" "private_route_table_1" {
 resource "aws_route_table" "private_route_table_2" {
   vpc_id = aws_vpc.main.id
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.nat_gateway_2.id
   }
 }
@@ -38,22 +38,22 @@ resource "aws_route_table_association" "subnet_association_2" {
 
 # Associate private route table 1 with subnets 1 and 3
 resource "aws_route_table_association" "private_subnet_association_1" {
-  subnet_id = aws_subnet.private_subnet_1.id
+  subnet_id      = aws_subnet.private_subnet_1.id
   route_table_id = aws_route_table.private_route_table_1.id
 }
 
 resource "aws_route_table_association" "private_subnet_association_3" {
-  subnet_id = aws_subnet.private_subnet_3.id
+  subnet_id      = aws_subnet.private_subnet_3.id
   route_table_id = aws_route_table.private_route_table_1.id
 }
 
 # Associate private route table 2 with subnets 2 and 4
 resource "aws_route_table_association" "private_subnet_association_2" {
-  subnet_id = aws_subnet.private_subnet_2.id
+  subnet_id      = aws_subnet.private_subnet_2.id
   route_table_id = aws_route_table.private_route_table_2.id
 }
 
 resource "aws_route_table_association" "private_subnet_association_4" {
-  subnet_id = aws_subnet.private_subnet_4.id
+  subnet_id      = aws_subnet.private_subnet_4.id
   route_table_id = aws_route_table.private_route_table_2.id
 }
---
+++ security_groups.tf
@@ -0,0 +1,85 @@
+# Application Load Balancer security group
+resource "aws_security_group" "alb_security_group" {
+  name        = "alb_security_group"
+  description = "Application security group"
+  vpc_id      = aws_vpc.main.id
+}
+
+# ALB accepts traffic from the internet
+resource "aws_security_group_rule" "alb_security_group_rules" {
+  count = length(var.alb_ingress_rules)
+
+  type              = "ingress"
+  from_port         = var.alb_ingress_rules[count.index].from_port
+  to_port           = var.alb_ingress_rules[count.index].to_port
+  protocol          = var.alb_ingress_rules[count.index].protocol
+  cidr_blocks       = [var.alb_ingress_rules[count.index].cidr_blocks]
+  description       = var.alb_ingress_rules[count.index].description
+  security_group_id = aws_security_group.alb_security_group.id
+}
+
+# Web server security group
+resource "aws_security_group" "web_server_security_group" {
+  name        = "web_server_security_group"
+  description = "EC2 instance security group"
+  vpc_id      = aws_vpc.main.id
+}
+
+# Severs accept traffic only from ALB
+resource "aws_security_group_rule" "web_server_security_group_rules" {
+  count = length(var.web_server_ingress_rules)
+
+  type                     = "ingress"
+  from_port                = var.web_server_ingress_rules[count.index].from_port
+  to_port                  = var.web_server_ingress_rules[count.index].to_port
+  protocol                 = var.web_server_ingress_rules[count.index].protocol
+  source_security_group_id = aws_security_group.alb_security_group.id
+  description              = var.web_server_ingress_rules[count.index].description
+  security_group_id        = aws_security_group.web_server_security_group.id
+}
+
+# Database security group
+resource "aws_security_group" "database_security_group" {
+  name        = "database_security_group"
+  description = "Database layer security group"
+  vpc_id      = aws_vpc.main.id
+}
+
+# Database accessible only by application server
+resource "aws_security_group_rule" "database_security_group_rules" {
+  type                     = "ingress"
+  from_port                = 3306
+  to_port                  = 3306
+  protocol                 = "TCP"
+  source_security_group_id = aws_security_group.web_server_security_group.id
+  description              = "MYSQL/Aurora access"
+  security_group_id        = aws_security_group.database_security_group.id
+}
+
+# Elastic File System security group
+resource "aws_security_group" "efs_security_group" {
+  name        = "efs_security_group"
+  description = "Elastic File System security group"
+  vpc_id      = aws_vpc.main.id
+}
+
+# EFS accessible only by application server
+resource "aws_security_group_rule" "efs_security_group_rules_1" {
+  type                     = "ingress"
+  from_port                = 2049
+  to_port                  = 2049
+  protocol                 = "TCP"
+  source_security_group_id = aws_security_group.web_server_security_group.id
+  description              = "EFS web server access"
+  security_group_id        = aws_security_group.efs_security_group.id
+}
+
+resource "aws_security_group_rule" "efs_security_group_rules_1" {
+  type                     = "ingress"
+  from_port                = 2049
+  to_port                  = 2049
+  protocol                 = "TCP"
+  source_security_group_id = aws_security_group.efs_security_group.id
+  description              = "EFS securty group access - self"
+  security_group_id        = aws_security_group.efs_security_group.id
+}
