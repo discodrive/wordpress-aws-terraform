dfc15c853f886544b500e483d68bdc90088151e5
Lee Aplin <lee@substrakt.com>
2023-06-06 16:04:23 +0100

    ADD: NAT gateways
    - Add and configure NAT gateways in the two public subnets
    - Refactor Private subnets to be more verbose rather than trying to loop through a map

---
+++ routing.tf
@@ -0,0 +1,59 @@
+# Configure the default route table. Terraform doesn't create a new route table, but adopts the default, clears existing defined routes and uses only rules specified in the terraform configuration.
+resource "aws_default_route_table" "public_route_table" {
+  default_route_table_id = aws_vpc.main.default_route_table_id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main_gateway.id
+  }
+}
+
+# Create route tables for Private subnets - This needs to be refactored to use a loop rather than repeating code
+resource "aws_route_table" "private_route_table_1" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gateway_1.id
+  }
+}
+
+resource "aws_route_table" "private_route_table_2" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gateway_2.id
+  }
+}
+
+# Associate public subnets with route table
+resource "aws_route_table_association" "subnet_association_1" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_default_route_table.public_route_table.id
+}
+
+resource "aws_route_table_association" "subnet_association_2" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_default_route_table.public_route_table.id
+}
+
+# Associate private route table 1 with subnets 1 and 3
+resource "aws_route_table_association" "private_subnet_association_1" {
+  subnet_id = aws_subnet.private_subnet_1.id
+  route_table_id = aws_route_table.private_route_table_1.id
+}
+
+resource "aws_route_table_association" "private_subnet_association_3" {
+  subnet_id = aws_subnet.private_subnet_3.id
+  route_table_id = aws_route_table.private_route_table_1.id
+}
+
+# Associate private route table 2 with subnets 2 and 4
+resource "aws_route_table_association" "private_subnet_association_2" {
+  subnet_id = aws_subnet.private_subnet_2.id
+  route_table_id = aws_route_table.private_route_table_2.id
+}
+
+resource "aws_route_table_association" "private_subnet_association_4" {
+  subnet_id = aws_subnet.private_subnet_4.id
+  route_table_id = aws_route_table.private_route_table_2.id
+}
--- vpc.tf
+++ vpc.tf
@@ -17,7 +17,7 @@ resource "aws_eip" "elastic_ip_2" {
   public_ipv4_pool = "amazon"
 }
 
-# NAT gateways for 
+# NAT gateways
 resource "aws_nat_gateway" "nat_gateway_1" {
   allocation_id = aws_eip.elastic_ip_1.id
   subnet_id     = aws_subnet.public_subnet_1.id
@@ -35,28 +35,3 @@ resource "aws_nat_gateway" "nat_gateway_2" {
     Name = "NAT Gateway Public Subnet 2"
   }
 }
-
-# Configure the default route table. Terraform doesn't create a new route table, but adopts the default, clears existing defined routes and uses only rules specified in the terraform configuration.
-resource "aws_default_route_table" "public_route_table" {
-  default_route_table_id = aws_vpc.main.default_route_table_id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main_gateway.id
-  }
-}
-
-# Associate public subnets with route table
-resource "aws_route_table_association" "subnet_association_1" {
-  subnet_id      = aws_subnet.public_subnet_1.id
-  route_table_id = aws_default_route_table.public_route_table.id
-}
-
-resource "aws_route_table_association" "subnet_association_2" {
-  subnet_id      = aws_subnet.public_subnet_2.id
-  route_table_id = aws_default_route_table.public_route_table.id
-}
-
-# Create route tables for Private subnets
-# Associate private route table 1 with subnets 1 and 3
-# Associate private route table 2 with subnets 2 and 4
