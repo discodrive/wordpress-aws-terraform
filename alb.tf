aa98d69a17348edf761bbcf89080c3935efc48cc
Lee Aplin <lee@substrakt.com>
2023-07-11 11:00:53 +0100

    ADD: Application load balancer and register ec2 instances as targets

---
+++ alb.tf
@@ -0,0 +1,45 @@
+resource "aws_lb" "application_load_balancer" {
+    name = "application_load_balancer"
+    internal = false
+    ip_address_type = "ipv4"
+    load_balancer_type = "application"
+    security_groups = [aws_security_group.alb_security_group.id]
+    subnets = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
+}
+
+resource "aws_lb_listener" "alb_listener" {
+    load_balancer_arn = aws_lb.application_load_balancer.arn
+    port = 80
+    protocol = "HTTP"
+    default_action {
+        type = "forward"
+        forward {
+            target_group {
+                arn = aws_lb_target_group.alb_target_group.arn
+            }
+            stickiness {
+                enabled  = true
+                duration = 1
+            }
+        }
+    }
+}
+
+resource "aws_lb_target_group" "alb_target_group" {
+    name = "alb_tg"
+    port = 80
+    protocol = "HTTP"
+    vpc_id = aws_vpc.main.id
+}
+
+resource "aws_lb_target_group_attachment" "server_1a_attachment" {
+    target_group_arn = aws_lb_target_group.alb_target_group.arn
+    target_id = aws_instance.server_1a.id
+    port = 80
+}
+
+resource "aws_lb_target_group_attachment" "server_1b_attachment" {
+    target_group_arn = aws_lb_target_group.alb_target_group.arn
+    target_id = aws_instance.server_1b.id
+    port = 80
+}
